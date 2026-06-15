e83182d7e79de213cd5b74981d9ce1b13f3ffec8
juancra264 <juancra264@hotmail.com>
2026-06-15 13:38:08 -0500

    fix: update NoMachine installation to use .deb package format

--- install_debian.sh
+++ install_debian.sh
@@ -468,9 +468,9 @@ f_linux_nx() {
   read -r -p "Continue? [y/N]" -n 1
   echo # (optional) move to a new line
   if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-  	wget -P $HOME https://web9001.nomachine.com/download/9.6/Linux/nomachine_9.6.3_1_x86_64.rpm
-    sudo dpkg -i $HOME/nomachine_9.6.3_1_x86_64.rpm
-    rm -rf $HOME/nomachine_9.6.3_1_x86_64.rpm
+  	wget -P $HOME https://web9001.nomachine.com/download/9.6/Linux/nomachine_9.6.3_1_amd64.deb  
+    sudo dpkg -i $HOME/nomachine_9.6.3_1_amd64.deb
+    rm -rf $HOME/nomachine_9.6.3_1_amd64.deb
   fi
 }
 
