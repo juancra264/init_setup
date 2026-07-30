66c3f79d193933b161de8de23b0c6d1f32fe2c05
juancra264 <juancra264@hotmail.com>
2026-07-30 12:03:03 -0500

    update TerraformRole privileges to include additional permissions

--- create_proxmox_api.sh
+++ create_proxmox_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Create a Custom Role
-pveum role add TerraformRole -privs "VM.Allocate VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Console VM.Monitor VM.PowerMgmt Datastore.AllocateSpace Datastore.Audit SDN.Use"
+pveum role add TerraformRole -privs "VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt Datastore.AllocateSpace Datastore.Audit Pool.Allocate SDN.Use Sys.Audit Sys.Console Sys.Modify Sys.PowerMgmt"
 
 # Create the dedicated Terraform user
 pveum user add terraform-prov@pve --comment "Terraform Provisioner"
